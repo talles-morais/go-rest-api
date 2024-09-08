5930b8ca4dbbdb0df440155d945d72fe5e74b9cd
Talles <talves2503@gmail.com>
2024-09-08 18:24:30 -0300

    feat: add middleware

--- routes/routes.go
+++ routes/routes.go
@@ -4,18 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/talles-morais/go-rest-api/controllers"
+	"github.com/talles-morais/go-rest-api/middleware"
 )
 
 func HandleRequest() {
 	router := mux.NewRouter()
-
+	router.Use(middleware.HeaderSetupMiddleware)
 	router.HandleFunc("/", controllers.Home)
 	router.HandleFunc("/api/personalities", controllers.AllPersonalities).Methods("Get")
 	router.HandleFunc("/api/personalities", controllers.CreatePersonality).Methods("Post")
 	router.HandleFunc("/api/personalities/{id}", controllers.OnePersonality).Methods("Get")
 	router.HandleFunc("/api/personalities/{id}", controllers.DeletePersonality).Methods("Delete")
 	router.HandleFunc("/api/personalities/{id}", controllers.EditPersonality).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(router)))
 }
